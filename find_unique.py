3719362df7084fdab13986e7cf1da5a794c98bad
ALINE <hoshinoaline@yahoo.com.br>
2023-03-05 21:28:32 -0300

    feature- exercise find_it from codewars

--- find_unique.py
+++ find_unique.py
@@ -1,26 +1,52 @@
-def find_uniq(arr):
-    count_number = dict()
-    for item in arr:
-        if item in count_number:
-            count_number[item] += 1
+# def find_uniq(arr):
+#     count_number = dict()
+#     for item in arr:
+#         if item in count_number:
+#             count_number[item] += 1
+#         else:
+#             count_number[item] = 1
+#     for item in count_number:
+#         if count_number[item] == 1:
+#             return item
+#     return
+
+
+# print(find_uniq([1, 1, 1, 2, 1]))
+
+
+# def find_uniq2(arr):
+#     a, b = set([1, 1, 1, 2, 1])
+#     return a if arr.count(a) == 1 else b
+
+
+# find_uniq2()
+# number = {"1": 2, "2": 1}
+# print("1" in number)
+# number["3"] = 1
+# print(number)
+
+
+def find_it(seq):
+    count_num_dict = dict()
+    for num in seq:
+        if num in count_num_dict:
+            count_num_dict[num] += 1
         else:
-            count_number[item] = 1
-    for item in count_number:
-        if count_number[item] == 1:
-            return item
+            count_num_dict[num] = 1
+    print(count_num_dict)
+    for num in count_num_dict:
+        if count_num_dict[num] % 2 != 0:
+            return num
     return
 
 
-print(find_uniq([1, 1, 1, 2, 1]))
+print(find_it([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]))
 
 
-def find_uniq2(arr):
-    a, b = set([1, 1, 1, 2, 1])
-    return a if arr.count(a) == 1 else b
+def find_it2(seq):
+    for i in seq:
+        if seq.count(i) % 2 != 0:
+            return i
 
 
-find_uniq2()
-number = {"1": 2, "2": 1}
-print("1" in number)
-number["3"] = 1
-print(number)
+print(find_it2([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]))
