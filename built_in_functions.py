1612789f3390e879987a79fd9a808e811a6e720b
ALINE <hoshinoaline@yahoo.com.br>
2023-06-26 10:56:48 -0300

    adiciona estudo sobre reduce

--- built_in_functions.py
+++ built_in_functions.py
@@ -1,4 +1,14 @@
+from functools import reduce
 a = [1, 2, 3, 4]
-b = [5, 6, 7, 8]
+b = [5, 62, 7, 8]
 
 print(list(map(lambda x, y: x+y, a, b)))
+
+
+def soma(x, y):
+    return x + y
+
+
+lst = [1, 15, 40, 7]
+print(reduce(soma, b))
+print(reduce(lambda a, b: a if a > b else b, lst))
